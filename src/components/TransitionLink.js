a83d59a611d3c93a3077f383599bd2c57dcf6224
Andrew Usher <andrewusher00@gmail.com>
2019-10-13 09:48:30 -0500

    refactor(TransitionLink): change color of paintDrip effect

--- src/components/TransitionLink.js
+++ src/components/TransitionLink.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
 
-const TransitionLink = props => <AniLink paintDrip duration={0.7} {...props} />
+const TransitionLink = props => <AniLink paintDrip duration={0.7} hex="#2a1a5e" {...props} />
 
 export { TransitionLink }
