9b2a9645929205168131d9353ca57d0f4b3c1dae
Andrew Usher <andrewusher00@gmail.com>
2019-10-13 21:39:10 -0500

    refactor(lint): turn off react/prop-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,4 +9,7 @@ module.exports = {
   globals: {
     graphql: false,
   },
+  rules: {
+    'react/prop-types': 0,
+  },
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Layout from '../components/Layout'
 
-const NotFoundPage = ({ location }) => console.log(location) || (
+const NotFoundPage = ({ location }) => (
   <Layout location={location}>
     <h1>Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
