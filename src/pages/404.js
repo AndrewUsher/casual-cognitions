84a7fde160fd5b6ba4da0506edc73bf280481cea
Andrew Usher <andrewusher00@gmail.com>
2020-04-20 11:12:33 -0500

    refactor(404): remove prop-types

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Layout } from '../components/Layout'
 
 const NotFoundPage = ({ location }) => (
@@ -8,11 +7,5 @@ const NotFoundPage = ({ location }) => (
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
-
-NotFoundPage.propTypes = {
-  location: PropTypes.shape({
-    pathname: PropTypes.string
-  })
-}
 
 export default NotFoundPage
